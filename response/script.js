6bac204a69b655c050ced0cf5dedb62d0b9a3069
Peixoto1214 <danpeixoto14@gmail.com>
2022-03-13 22:11:41 -0400

    project - portfolio page

--- response/script.js
+++ response/script.js
@@ -1,1 +1,20 @@
-console.log('im here');
+// link nav toggle
+
+document.addEventListener('DOMContentLoaded', function(event){
+      
+    // show mobile nav menu
+
+    let navShow = document.querySelector('.navShow')
+    //when I click the burger
+    document.querySelector('.dp-nav-toggle').addEventListener('click', function(e){
+        e.preventDefault();
+    // target the dp-nav-menu-mobile class
+    document.querySelector('.dp-nav-menu-mobile').classList.toggle('navShow')
+
+
+
+    });
+    
+   
+
+});
