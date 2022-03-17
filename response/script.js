aee112212e0a6b907b9831c39903c2b8506567fc
Peixoto1214 <danpeixoto14@gmail.com>
2022-03-16 22:06:18 -0400

    added JS functionality to portfolio page - now cards rotate through showing a large card on the click

--- response/script.js
+++ response/script.js
@@ -14,7 +14,35 @@ document.addEventListener('DOMContentLoaded', function(event){
 
 
     });
+
+    //create variables for each class
+    let smallCards = document.getElementsByClassName('dp-grid-containers');
     
+    //create loop for all the cards
+    for (let i = 0; i < smallCards.length; i++) {
+    //my end goal is to swap my regular class for my large class
+    smallCards[i].addEventListener('click', function(e){
+    
+    //add click event targeting class
+    
+    //when I click on a small card
+       
+    for (let r = 0; r < smallCards.length; r++) {
+        
+            smallCards[r].classList.remove('dp-grid-containers-large');
+
+
+            }       
+            //the large card is added
+            this.classList.add('dp-grid-containers-large');
+         
+        });
+           
+
+    };
+       
+
    
 
 });
+console.log('Oi Amigo');
